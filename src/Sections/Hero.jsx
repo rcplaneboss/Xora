65efb3094603aadafa6194b8293ab0bc357b1479
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-01-24 19:26:17 +0100

    Added hero image and call-to-action button

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,8 +1,9 @@
 import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../Components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amper-400">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -17,8 +18,16 @@ const Hero = () => {
               Learn, and suprisingly powerful.
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-              Text
+              <Button icon="/images/zap.svg">Try it now!</Button>
             </LinkScroll>
+          </div>
+
+          <div className="absolute -top-32 left-[calc(30%)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt=""
+            />
           </div>
         </div>
       </Element>
